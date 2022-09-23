88837b9d17680b46afe1b1c5c5b3bc15410cf32c
i <iraklishanava01@gmail.com>
2022-09-23 12:33:50 +0400

    Added a decoder for html-entities

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Typography, Card, CardContent, CardMedia } from '@mui/material'
-import { CheckCircle } from '@mui/icons-material'
-
+import { CheckCircle } from '@mui/icons-material' 
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utilities/constants';
+import { decode } from 'html-entities'
  
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
   <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
@@ -15,12 +15,12 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
     <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
         <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
-          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          {decode(snippet?.title.slice(0, 60)) || demoVideoTitle.slice(0, 60)}
         </Typography>
       </Link>
       <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
         <Typography variant="subtitle2" color="gray">
-          {snippet?.channelTitle || demoChannelTitle}
+          {decode(snippet?.channelTitle) || demoChannelTitle}
           <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
         </Typography>
       </Link>
